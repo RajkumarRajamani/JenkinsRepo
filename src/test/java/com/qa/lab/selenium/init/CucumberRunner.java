b077c77099be3ac8d50bf0074e671d4d8a302060
RajkumarRajamani <rajoviyaa.s@gmail.com>
2023-04-17 00:23:25 +0530

    commit

--- src/test/java/com/qa/lab/selenium/init/CucumberRunner.java
+++ src/test/java/com/qa/lab/selenium/init/CucumberRunner.java
@@ -1,8 +1,12 @@
 package com.qa.lab.selenium.init;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
 		
 		plugin = {
