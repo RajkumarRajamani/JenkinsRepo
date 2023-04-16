d020fc09acfed4ea367a75f989cb771b1d26a002
RajkumarRajamani <rajoviyaa.s@gmail.com>
2023-04-16 23:32:42 +0530

    commit

--- src/main/java/com/qa/lab/selenium/init/CucumberRunner.java
+++ src/main/java/com/qa/lab/selenium/init/CucumberRunner.java
@@ -5,7 +5,11 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		
-		plugin = {"pretty", "html:TestOutput/cucumber-report/cucumber-pretty.html"},
+		plugin = {
+				"pretty",
+				"html:TestOutput/cucumber-report/cucumber-pretty.html",
+				"json:TestOutput/cucumber-report/cucumber-json.json"
+				},
 		features = {"src/test/java/com/qa/lab/selenium/features/"},
 		glue = {"com.qa.lab.selenium.stepdef"},
 		dryRun = false,
